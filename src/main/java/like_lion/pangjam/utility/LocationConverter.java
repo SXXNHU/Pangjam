e8ba0d769a236edd6d6cc2acd2ae37d634ebde9a
SXXHU <dofqls0127@gmail.com>
2025-05-02 17:37:16 +0900

    Feat: LocationResponseDto 변환 메서드 추가 (#2)
    - toLocationResponseDto 메서드 추가 : Location 객체를 기반으로 기본
      응답Dto 리스트 생성
    - toDeatilDto 메서드 추가 : 최신 리뷰 5개를 포함하여 DetailResponseDto
      생성

--- src/main/java/like_lion/pangjam/utility/LocationConverter.java
+++ src/main/java/like_lion/pangjam/utility/LocationConverter.java
@@ -1,19 +1,26 @@
 package like_lion.pangjam.utility;
 
 import like_lion.pangjam.domain.Location;
+import like_lion.pangjam.domain.LocationReview;
 import like_lion.pangjam.dto.location.LocationResponseDto;
+import like_lion.pangjam.repository.LocationReviewRepository;
 
 import java.util.List;
 
-    public class LocationConverter {
-        public static LocationResponseDto toLocationResponseDTO(List<Location> locations) {
-            List<LocationResponseDto.LocationInfo> locationInfos = locations.stream()
-                    .map(LocationResponseDto.LocationInfo::from)
-                    .toList();
+public class LocationConverter {
 
-            return new LocationResponseDto(locationInfos);
-        }
+    private static LocationReviewRepository locationReviewRepository;
+
+    public static LocationResponseDto toLocationResponseDTO(List<Location> locations) {
+        List<LocationResponseDto.LocationInfo> locationInfos = locations.stream()
+                .map(LocationResponseDto.LocationInfo::from)
+                .toList();
+
+        return new LocationResponseDto(locationInfos);
     }
 
-
+    public static LocationResponseDto.Detail toDetailDTO(Location location) {
+        List<LocationReview> locationReviews = locationReviewRepository.findTop5ByLocation_LocationIdOrderByCreatedAtDesc(location.getLocationId());
+        return LocationResponseDto.Detail.from(location,locationReviews);
+    }
 }
