ad9d3dd22eb56097b5a2e8daa612941d5f5b6129
SXXHU <dofqls0127@gmail.com>
2025-05-02 17:34:27 +0900

    Feat: 최신 리뷰 조회용 쿼리 메서드 추가 (#2)
    - 특정 식당의 리뷰를 최신순으로 최대 5개까지 조회하는 메서드 추가
    - getLocationDetail 기능에서 활용될 예정

--- src/main/java/like_lion/pangjam/repository/LocationReviewRepository.java
+++ src/main/java/like_lion/pangjam/repository/LocationReviewRepository.java
@@ -3,5 +3,10 @@ package like_lion.pangjam.repository;
 import like_lion.pangjam.domain.LocationReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LocationReviewRepository extends JpaRepository<LocationReview, Integer> {
+
+    List<LocationReview> findTop5ByLocation_LocationIdOrderByCreatedAtDesc(int locationId);
+
 }
