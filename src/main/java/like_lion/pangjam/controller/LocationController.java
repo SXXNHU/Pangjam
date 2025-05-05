0dcbddc071708da6c4d1b8d1ed87f127aacc336d
SXXHU <dofqls0127@gmail.com>
2025-05-05 18:33:03 +0900

    Feat: 식당 카테고리/이름 조회 API 엔드포인트 추가 (#3)
    - 카테고리 기준 조회용 GET/{category} 엔드포인트 구현
    - 이름 기준 조회용 엔트포인트 구현
    - LocationService의 조회 메서드 호출 후 ResposeEntity로 반환

--- src/main/java/like_lion/pangjam/controller/LocationController.java
+++ src/main/java/like_lion/pangjam/controller/LocationController.java
@@ -1,4 +1,58 @@
 package like_lion.pangjam.controller;
 
+import like_lion.pangjam.dto.location.LocationResponseDto;
+import like_lion.pangjam.dto.global.SliceResponseDto;
+import like_lion.pangjam.service.LocationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Slice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/locations")
+@RequiredArgsConstructor
 public class LocationController {
+
+    private final LocationService locationService;
+
+@GetMapping
+public ResponseEntity<SliceResponseDto<LocationResponseDto.LocationInfo>> getLocations(
+        @RequestParam(required = false) Integer lastLocationId
+) {
+    Slice<LocationResponseDto.LocationInfo> slice = locationService.getAllLocation(lastLocationId);
+    
+    SliceResponseDto<LocationResponseDto.LocationInfo> response = SliceResponseDto.of(
+            slice.getContent(),
+            slice.hasNext(),
+            0,
+            15
+    );
+    
+    return ResponseEntity.ok(response);
+}
+
+    //카테고리별 조회
+    @GetMapping("/{category}")
+    public ResponseEntity<LocationResponseDto> getLocationByCategory(@PathVariable String category)
+    {
+        LocationResponseDto locations = locationService.getLocationByCategory(category);
+        return ResponseEntity.status(HttpStatus.OK).body(locations);
+    }
+
+    //이름으로 조회
+    @GetMapping("/{name}")
+    public ResponseEntity<LocationResponseDto> getLocationByName(@PathVariable String name)
+    {
+        LocationResponseDto locations = locationService.getLocationByName(name);
+        return ResponseEntity.status(HttpStatus.OK).body(locations);
+    }
+
+    // 상세 정보 조회
+    @GetMapping("/{locationId}")
+    public ResponseEntity<LocationResponseDto.Detail> geLocationDetail(@PathVariable int locationId)
+    {
+        LocationResponseDto.Detail locationDetail =locationService.getLocationDetail(locationId);
+        return ResponseEntity.status(HttpStatus.OK).body(locationDetail);
+    }
 }
