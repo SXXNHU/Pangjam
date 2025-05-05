d4409c1c0bde3de1f69323aea0a76b7b95495bae
SXXHU <dofqls0127@gmail.com>
2025-05-05 19:06:17 +0900

    Feat: 무한 스크롤 대응 SliceResponseDto 추가 (#1)
    - content, hasNext, currentPage, size 필드 포함
    - 페이징 정보를 담은 정적 팩토리 메서드 of() 제공
    - 무한 스크롤 API 응답에 활용 예정

--- src/main/java/like_lion/pangjam/dto/global/SliceResponseDto.java
+++ src/main/java/like_lion/pangjam/dto/global/SliceResponseDto.java
@@ -1,4 +1,22 @@
 package like_lion.pangjam.dto.global;
 
-public class SliceResponseDto {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SliceResponseDto<T> {
+    private List<T> content;
+    private boolean hasNext;
+    private int currentPage;
+    private int size;
+
+    // 페이징 정보만 담는 생성자 추가
+    public static <T> SliceResponseDto<T> of(List<T> content, boolean hasNext, int currentPage, int size) {
+        return new SliceResponseDto<>(content, hasNext, currentPage, size);
+    }
 }
