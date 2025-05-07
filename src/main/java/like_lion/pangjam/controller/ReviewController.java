2ea034b9b5deff5152d6598cf27c9855aaeef70b
SXXHU <dofqls0127@gmail.com>
2025-05-07 10:55:30 +0900

    Feat(controller): 학식 리뷰 생성 API 엔드포인트 추가
    - POST 요청을 처리하는 createReview 메서드 구현

--- src/main/java/like_lion/pangjam/controller/ReviewController.java
+++ src/main/java/like_lion/pangjam/controller/ReviewController.java
@@ -1,7 +1,12 @@
 package like_lion.pangjam.controller;
 
+import like_lion.pangjam.dto.review.ReviewRequestDto;
+import like_lion.pangjam.dto.review.ReviewResponseDto;
 import like_lion.pangjam.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +17,13 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
+    //리뷰 생성
+    @PostMapping
+    public ResponseEntity<ReviewResponseDto> createReview(@RequestBody ReviewRequestDto reviewRequestDto) {
+        return ResponseEntity.ok(reviewService.createReview(reviewRequestDto));
+    }
+
+    
     //리뷰 전체 조회
 
-    //리뷰 작성
 }
