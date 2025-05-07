c7738d60d9908f60203e72696604eb3912be908c
SXXHU <dofqls0127@gmail.com>
2025-05-07 11:00:35 +0900

    Feat(controller): 학식 리뷰 조회 API 엔드포인트 추가
    - 누적된 모든 학식 리뷰를 조회하여 페이지로 반환

--- src/main/java/like_lion/pangjam/controller/ReviewController.java
+++ src/main/java/like_lion/pangjam/controller/ReviewController.java
@@ -4,11 +4,12 @@ import like_lion.pangjam.dto.review.ReviewRequestDto;
 import like_lion.pangjam.dto.review.ReviewResponseDto;
 import like_lion.pangjam.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/reviews")
@@ -23,7 +24,14 @@ public class ReviewController {
         return ResponseEntity.ok(reviewService.createReview(reviewRequestDto));
     }
 
-    
     //리뷰 전체 조회
+    @GetMapping
+    public ResponseEntity<List<ReviewResponseDto>> getReviews(int reviewId,
+                                                        @RequestParam(defaultValue = "0") int page,
+                                                        @RequestParam(defaultValue = "15") int size) {
 
+        Pageable pageable = PageRequest.of(page,size);
+        List<ReviewResponseDto> reviews = reviewService.getReviews(reviewId, pageable);
+        return ResponseEntity.ok(reviews);
+    }
 }
